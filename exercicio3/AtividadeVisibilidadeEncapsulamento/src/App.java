eb89a0197e16856112014d0e8331d769dc401da4
Renêr <91918580+renercrisostomo@users.noreply.github.com>
2022-09-20 23:05:44 -0300

    exercicio 3 e 4

---
+++ exercicio3/AtividadeVisibilidadeEncapsulamento/src/App.java
@@ -0,0 +1,130 @@
+import java.util.Date;
+import java.util.SimpleDateFormat;
+
+public class App {
+
+    public class Funcionario {
+
+        private String nome;
+        private String dataAdmissao;
+        private float salario;
+    
+        public Funcionario(String nome, String dataAdmissao, float salario) {
+            this.nome = nome;
+            this.dataAdmissao = dataAdmissao;
+            this.salario = salario;
+        }
+    
+        public String getNome() {
+            return nome;
+        }
+    
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+    
+        public String getDataAdmissao() {
+            return dataAdmissao;
+        }
+    
+        public void setDataAdmissao(String dataAdmissao) {
+            this.dataAdmissao = dataAdmissao;
+        }
+
+        public float getSalario() {
+            return salario;
+        }
+    
+        public void setSalario(float salario) {
+            this.salario = salario;
+        }
+
+    }
+    
+    public static void main(String[] args) throws Exception {
+        Funcionario funcionario = new Funcionario("João", "01/01/2020", "R$ 1000,00");
+        System.out.println(funcionario.getNome());
+        System.out.println(funcionario.getDataAdmissao());
+        System.out.println(funcionario.getSalario());   
+    }
+}
+
+/*
+ * # **Exercício 3**
+
+## Questão 1
+
+Crie uma classe pública denominada Funcionario. Esta classe deve possuir os atributos a
+seguir: nome, dataAdmissao e salario. Todos os atributos devem ser privados.
+
+## Questão 2
+
+Crie os getters e setters necessários da sua classe Funcionario, conforme o exemplo a seguir:
+
+```java
+class Funcionario {
+
+    private double salario;
+    //...
+    public double getSalario() {
+        return this.salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
+```
+
+**Não copie e cole!** Aproveite para praticar a sintaxe.
+
+## Questão 3
+
+Crie uma classe pública denominada Principal, que contém um método main. Neste método,
+crie 3 instâncias da classe Funcionario, fornecendo os atributos através dos métodos de acesso.
+Dica: use a classe `java.util.Date` para armazenar datas e a classe `java.text.SimpleDateFormat` para
+converter datas em texto para objetos `java.util.Date`.
+Referência útil: <https://docs.oracle.com/javase/8/docs/api/java/text/SimpleDateFormat.html>
+Ex:
+
+```java
+SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+Date d = sdf.parse("01/02/2015");
+```
+
+## Questão 4
+
+Adicione um atributo na classe Funcionario de tipo int que se chama identificador. Esse
+identificador deve ter um valor único para cada instância do tipo Funcionario. O primeiro
+Funcionario instanciado deve ter o identificador 1, o segundo 2, e assim por diante. Isto deve ser
+feito automaticamente na classe Funcionario, com o uso do construtor simples sem argumentos.
+Sugere-se utilizar um atributo auxiliar inteiro, privado e estático para armazenar o próximo
+identificador a ser utilizado neste processo de contagem.
+Crie um getter para o identificador. Precisamos de um setter?
+
+## Questão 5
+
+Adicione uma validação no método setSalario, de forma que não seja permitido adicionar um
+salário inferior a R$ 1.100,00.
+
+## Questão 6
+
+Adicione uma validação no método setDataAdmissao, de forma que não seja permitido adicionar
+uma data no futuro.
+
+## Observações
+
+### Assunto
+
+Visibilidade e encapsulamento.
+
+### Orientações
+
+A atividade deve ser executada individualmente e entregue através do ambiente Google Classroom.
+
+### Regras de criação dos programas
+
+Crie um novo projeto Java denominado AtividadeVisibilidadeEncapsulamento. As classes devem
+possuir os nomes informados nas questões. Ao final, o projeto deve ser exportado para um arquivo em
+formato ZIP.
+
+*/
---
+++ exercicio4/AtividadeSobrecargaDeMetodos/src/App.java
@@ -0,0 +1,104 @@
+import java.util.Date;
+import java.util.SimpleDateFormat;
+
+public class App {
+
+    public class Funcionario {
+
+        private String nome;
+        private String dataAdmissao;
+        private float salario;
+    
+        public Funcionario(String nome, String dataAdmissao, float salario) {
+            this.nome = nome;
+            this.dataAdmissao = dataAdmissao;
+            this.salario = salario;
+        }
+
+        public void tirarFerias() {
+            System.out.println("de férias por 30 dias");
+        }
+
+        public void tirarFerias(int dias) {
+            System.out.println(String.format("de férias por %s dias", dias));
+        }
+    
+        public String getNome() {
+            return nome;
+        }
+    
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+    
+        public String getDataAdmissao() {
+            return dataAdmissao;
+        }
+    
+        public void setDataAdmissao(String dataAdmissao) {
+            this.dataAdmissao = dataAdmissao;
+        }
+
+        public float getSalario() {
+            return salario;
+        }
+    
+        public void setSalario(float salario) {
+            this.salario = salario;
+        }
+
+    }
+
+    public class Relogio {
+
+        public void inicializar(int hora, int minuto, int segundo) {
+            System.out.println(String.format("%s:%s:%s", hora, minuto, segundo));   
+        }
+        public void inicializar(int hora, int minuto) {
+            System.out.println(String.format("%s:%s:01", hora, minuto));   
+        }
+        public void inicializar(int hora) {
+            System.out.println(String.format("%s:01:01", hora));   
+        }
+
+    }
+    
+    public static void main(String[] args) throws Exception {
+        Funcionario funcionario = new Funcionario("João", "01/01/2020", 1000);
+        System.out.println(funcionario.getNome());
+        System.out.println(funcionario.getDataAdmissao());
+        System.out.println(funcionario.getSalario());   
+        funcionario.tirarFerias();
+        funcionario.tirarFerias(15);
+        Relogio relogio = new Relogio();
+        relogio.inicializar(10, 20, 30);
+        relogio.inicializar(10, 20);
+        relogio.inicializar(10);
+    }
+}
+
+/*
+# **Exercício 4**
+
+## Questão 1
+
+Utilizando os códigos criados na atividade sobre visibilidade e encapsulamento, altere a classe
+Funcionario de acordo com as instruções a seguir:
+
+1. Adicione o método `void tirarFerias()`, cujas instruções internas imprimem uma mensagem informando que o funcionário irá tirar férias de 30 dias
+2. Adicione o método `void tirarFerias(int dias)`, cujas instruções internas imprimem uma mensagem informando que o funcionário irá tirar a quantidade de dias informada de férias
+3. Organize estes dois métodos de forma que as instruções responsáveis por imprimir a mensagem estejam localizadas em apenas um dos métodos criados, fazendo com que um método chame o outro método
+4. Crie uma classe para demonstrar o funcionamento dos métodos criados
+
+Você percebeu como pode utilizar a sobrecarga para organizar o seu código?
+
+## Questão 2
+
+Crie uma classe denominada Relogio de acordo com as instruções a seguir:
+
+1. Crie um método `void inicializar`, que recebe: hora, minuto e segundo
+2. Crie outro método inicializar, que recebe somente a hora e o minuto, inicializando o segundo com 1
+3. Crie outro método inicializar, que recebe somente a hora, inicializando o minuto e o segundo com 1
+4. Crie uma classe para demonstrar o funcionamento dos métodos criados
+
+*/
