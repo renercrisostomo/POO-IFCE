4ea1f07faaab79747fa1b8414d719bcfa6d75602
Renêr <91918580+renercrisostomo@users.noreply.github.com>
2022-09-21 19:49:27 -0300

    exercício 5

---
+++ exercicio5/AtividadeConstrutores/src/App.java
@@ -0,0 +1,35 @@
+
+public class App {
+}
+
+/*
+# **Exercício 5**
+
+## Questão 1
+
+Para o caso de estudo a seguir, faça o que se pede:
+
+1. Crie o diagrama de classes das entidades identificadas utilizando um editor UML, observando em especial a declaração dos construtores
+2. Implemente o código-fonte equivalente às classes identificadas e crie um programa que demonstre o seu uso
+
+### **Estação ferroviária**
+
+Uma estação ferroviária é composta por uma ou mais linhas ferroviárias. Ao criar uma estação
+ferroviária, obrigatoriamente devem ser definidas as linhas ferroviárias que irão compor sua
+estrutura. Em uma linha ferroviária podem estar estacionados diversos recursos ferroviários, que
+opcionalmente podem ser informados durante a criação da estação ferroviária.
+Recursos ferroviários podem ser vagões, locomotivas ou trens. Um trem é formado por vagões e
+locomotivas que devem ser informados durante a criação do trem. Uma estação ferroviária tem
+uma sigla e uma descrição. Uma linha ferroviária tem um número (que a diferencia de outra linha
+dentro da mesma estação), uma extensão em metros e uma descrição. Um vagão é descrito por
+um número de série, tipo, capacidade de carga, comprimento entre testeiras e comprimento dos
+engates, podendo ser opcionalmente informados durante a sua criação.
+Uma locomotiva é descrita por um número de série, capacidade de tração e comprimento, também
+podendo ser opcionalmente informados durante a sua criação. Um trem é descrito por um prefixo
+(ex: NAG1010) e data de formação. Um trem é formado em uma estação ferroviária de origem e
+tem como destino uma outra estação ferroviária, ou seja, a estação de origem não pode ser igual à
+estação de destino (também deve ser informado durante a criação). Um trem não pode ter mais do
+que 150 recursos (vagões e locomotivas).
+
+**Para testar seu código-fonte, crie um programa executável que demonstre a criação de duas estações ferroviárias com uma linha ferroviária interligando as duas. Esta linha deve conter um trem, formado por uma locomotiva e três vagões.**
+*/
